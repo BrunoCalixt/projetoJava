6ade4b1eb13ecce97bdc9b59251a8794a8cddac3
Bruno Henrique De Souza Calixto <brunohcalixto@hotmail.com>
2022-08-31 20:34:35 -0300

    Alteração

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,7 +4,7 @@ public class PrimeiraClasseJava {
  
 		public static void main(String[] args) {
 		
-			System.out.println("Escreve o texto no console");	
+			System.out.println("Primeiro Curso java teste ");	
 }
 	
 }
