b740c4885b0f57f7cc37a6cf89e95ca2a0b2bac9
Bruno Henrique De Souza Calixto <brunohcalixto@hotmail.com>
2022-09-13 22:09:08 -0300

    Entrada de dados

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,10 +1,22 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
- 
-		public static void main(String[] args) {
+
+	public static void main(String[] args) {
+
+		String carros = JOptionPane.showInputDialog("Informe a Quantidade de Carros?");
+		String pessoas = JOptionPane.showInputDialog("Informe a Quantidade de Pessoas?");
 		
-			System.out.println("Primeiro Curso java teste ");	
-}
-	
+		double carroNumero = Double.parseDouble(carros);
+		double pessoasNumero = Double.parseDouble(pessoas);
+		
+		int divisao = (int)(carroNumero / pessoasNumero);
+		int resto = (int)(carroNumero % pessoasNumero); 
+		
+		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carros");
+
+	}
+
 }
